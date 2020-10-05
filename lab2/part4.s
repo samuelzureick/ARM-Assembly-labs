d7a07706c09fcf94fd3d9ae72c988a4fa0ef4d6d
Christos Kotselidis <christos.kotselidis@manchester.ac.uk>
2020-10-05 14:57:29 +0200

    Add lab questions

---
+++ lab2/part4.s
@@ -0,0 +1,54 @@
+; Age History
+
+	B  main
+
+born	DEFB "you were born in \0"
+were	DEFB "you were \0"
+in	DEFB " in \0"
+are	DEFB "you are \0"
+this	DEFB " this year\n\0"
+	ALIGN
+
+	; present = 2005 // Change this code
+	; birth = 1959   // Change this code
+	; year = 0       // Change this code
+	; age = 1        // Change this code
+
+main
+	; this code does print "you were born in " + str(birth) // DO NOT change the instructions below (except for part 5)
+	ADR R0, born
+	SVC 3
+	LDR R0, birth ; make sure this will work!
+	SVC 4
+	MOV R0, #10
+	SVC 0
+
+	; year = birth + 1 // Change this code
+
+	; while year != present //{ Change this code
+
+	; this code does print "you were " + str(age) + " in " + str(year) // DO NOT change the instructions below (except for part 5)
+	ADR R0, were
+	SVC 3
+	LDR R0, age ; make sure this will work!
+	SVC 4
+	ADR R0, in
+	SVC 3
+	LDR R0, year ; make sure this will work!
+	SVC 4
+	MOV R0, #10
+	SVC 0
+
+	;   year = year + 1 //Change this code
+	;   age = age + 1   //Change this code
+	; } //              //Change this code
+
+	; this code does print "you are " + str(age) + "this year" // DO NOT change the instructions below (except for part 5)
+	ADR R0, are
+	SVC 3
+	LDR R0, age ; make sure this will work!
+	SVC 4
+	ADR R0, this
+	SVC 3
+
+	SVC 2 ; stop
